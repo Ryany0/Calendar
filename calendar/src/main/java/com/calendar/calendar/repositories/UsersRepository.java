0dbda6ccca9bf0a7d9707c6b8e17dd20d5dba384
Ryan <109833217+Ryany0@users.noreply.github.com>
2025-10-06 21:54:32 -0700

    Rename from User to Users
    user is a reserved key for psql

--- calendar/src/main/java/com/calendar/calendar/repositories/UsersRepository.java
+++ calendar/src/main/java/com/calendar/calendar/repositories/UsersRepository.java
@@ -4,8 +4,6 @@ import com.calendar.calendar.Entities.Users;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface UserRepository extends CrudRepository<Users, Long> {
+public interface UsersRepository extends CrudRepository<Users, Long> {
 }
